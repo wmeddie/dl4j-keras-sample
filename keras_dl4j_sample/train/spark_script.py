71f1a1154a5addb8a05c667d871c84e1acb26dc3
Eduardo Gonzalez <me@wm-eddie.com>
2019-05-25 03:39:23 +0900

    add some debugging print statements.

--- keras_dl4j_sample/train/spark_script.py
+++ keras_dl4j_sample/train/spark_script.py
@@ -8,9 +8,11 @@ from keras.datasets import mnist
 from keras.utils import np_utils
 
 def process(sc, args):
+    print('importing elephas classes')
     from elephas.dl4j import ParameterSharingModel
     from elephas.utils.rdd_utils import to_java_rdd
 
+    print('doing some spark stuff')
     input_data = args if args else [1, 2, 3, 4, 5]
     distr_data = sc.parallelize(input_data)
     result = distr_data.collect()
@@ -25,9 +27,11 @@ def process(sc, args):
     x_test /= 255
     y_train = np_utils.to_categorical(y_train, 10)
     y_test = np_utils.to_categorical(y_test, 10)
-    
+
+    print('Converting numpy array to rdd.')
     train_rdd = to_java_rdd(sc, x_train, y_train, 64)
 
+    print('building model')
     model = Sequential()
     model.add(Dense(128, input_dim=784))
     model.add(Activation('relu'))
@@ -39,9 +43,12 @@ def process(sc, args):
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer=SGD(), metrics=['acc'])
 
+    print('converting model')
     spark_model = ParameterSharingModel(sc._jsc, model, batch_size=64, num_workers=4)
 
+    print('training model...')
     spark_model.fit_rdd(train_rdd, epochs=20)
+    print('training model finished.')
 
     score = spark_model.master_network.evaluate(x_test, y_test, verbose=2)
 
@@ -60,7 +67,10 @@ def main(args):
     Nd4j = sc._jvm.org.nd4j.linalg.factory.Nd4j
     print(Nd4j.eye(3).toString)
 
+    print('starting process...')
     process(sc, args)
+    print('process finished.')
+
     sc.stop()
 
 if __name__ == '__main__':
